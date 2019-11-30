b0341c73ad62881389a213d4cf4d858aa982b27b
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-11-30 02:37:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,12 +1,12 @@
 function forLoop (array){
   for (i=0;i<25;i++){
-  //  array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`)
+    array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`)
 //  if (i===1){
 //    array.push(`I am ${i} strange loop.`)
 //    }
 //    else{
 //      array.push(`I am ${i} strange loops.`)
-    }
+//    }
   }
   return array;
 }
